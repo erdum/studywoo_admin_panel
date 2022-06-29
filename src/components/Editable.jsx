256cb350ab4506c6a06656996064791e7328e6ff
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-06-29 12:31:36 +0500

    flexible input

--- src/components/Editable.jsx
+++ src/components/Editable.jsx
@@ -1,10 +1,18 @@
 import { useState, useEffect, useRef } from "react";
 
-import { InputGroup, Input, InputRightElement, Icon } from "@chakra-ui/react";
+import {
+	InputGroup,
+	Input,
+	InputRightElement,
+	FormLabel,
+	FormControl,
+	Icon,
+	Select,
+} from "@chakra-ui/react";
 
-import { FaRegEdit } from "react-icons/fa";
+import { FaPen } from "react-icons/fa";
 
-const Editable = ({ placeholder, value = "", onChange = () => {} }) => {
+const Editable = ({ name, value, onChange, label, type = "text" }) => {
 	const ref = useRef(null);
 	const [isEditable, setEditable] = useState(true);
 
@@ -15,24 +23,77 @@ const Editable = ({ placeholder, value = "", onChange = () => {} }) => {
 	}, [isEditable]);
 
 	return (
-		<InputGroup>
-			<Input
-				ref={ref}
-				onBlur={() => setEditable(true)}
-				isDisabled={isEditable}
-				type="text"
-				placeholder={placeholder}
-				fontWeight="medium"
-				_placeholder={{ color: "gray.500" }}
-				_disabled={{ color: "gray.500" }}
-				focusBorderColor="custom.primary"
-			/>
-			<InputRightElement
-				onClick={() => setEditable((prevState) => !prevState)}
-				children={<Icon color="gray.500" as={FaRegEdit} />}
-			/>
-		</InputGroup>
+		<FormControl w={{ md: "45%", lg: "64" }}>
+			<FormLabel
+				fontSize={{ base: "sm", md: "md" }}
+				color="gray.500"
+				htmlFor={name}
+			>
+				{label}
+			</FormLabel>
+			<InputGroup cursor="pointer">
+				<Input
+					id={name}
+					name={name}
+					ref={ref}
+					onKeyDown={({ key }) =>
+						key === "Escape" && setEditable(true)
+					}
+					value={value}
+					onChange={onChange}
+					onBlur={() => setEditable(true)}
+					isDisabled={isEditable}
+					type={type}
+					fontWeight="medium"
+					_placeholder={{ color: "gray.500" }}
+					_disabled={{ color: "gray.500" }}
+					focusBorderColor="custom.primary"
+				/>
+				<InputRightElement
+					onClick={() => setEditable((prevState) => !prevState)}
+					children={<Icon color="gray.500" as={FaPen} />}
+				/>
+			</InputGroup>
+		</FormControl>
 	);
 };
 
-export default Editable;
+const EditableSelect = ({ name, label, options, value, onChange }) => {
+	return (
+		<FormControl w={{ md: "45%", lg: "64" }}>
+			<FormLabel
+				fontSize={{ base: "sm", md: "ms" }}
+				color="gray.500"
+				htmlFor={name}
+			>
+				{label}
+			</FormLabel>
+			<Select
+				cursor="pointer"
+				id={name}
+				name={name}
+				value={value}
+				onChange={onChange}
+				fontWeight="medium"
+				color="gray.500"
+				focusBorderColor="custom.primary"
+				iconSize="md"
+				iconColor="gray.500"
+				icon={<Icon as={FaPen} />}
+				autoComplete="off"
+			>
+				<option disabled value="">
+					Select
+				</option>
+				{options &&
+					options.map(({ value, text }) => (
+						<option key={value} value={value}>
+							{text}
+						</option>
+					))}
+			</Select>
+		</FormControl>
+	);
+};
+
+export { Editable, EditableSelect };
--- src/pages/AccountSettings.jsx
+++ src/pages/AccountSettings.jsx
@@ -9,9 +9,16 @@ import request from "../helpers/request";
 // Custom Components
 import PageHeader from "../components/PageHeader";
 import { PageFieldSkeleton } from "../components/PageSkeleton";
-import Editable from "../components/Editable";
+import { Editable, EditableSelect } from "../components/Editable";
 
 const AccountSettings = () => {
+	const [fields, setFields] = useState({
+		name: "Erdum",
+		email: "",
+		password: "",
+		gender: "",
+		date_of_birth: "2004-10-29",
+	});
 	const [isLoading, setLoading] = useState(true);
 
 	useEffect(() => {
@@ -22,6 +29,17 @@ const AccountSettings = () => {
 			setLoading(false);
 		})();
 	}, []);
+
+	// useEffect(() => {
+	// 	console.log(fields);
+	// }, [fields]);
+
+	const handleChange = ({ target: { name, value } }) => {
+		setFields((prevState) => ({
+			...prevState,
+			[name]: value,
+		}));
+	};
 
 	return (
 		<>
@@ -36,8 +54,48 @@ const AccountSettings = () => {
 			<Box overflowY="auto" w="100%" h="100%">
 				{isLoading && <PageFieldSkeleton />}
 				{!isLoading && (
-					<Flex p="1">
-						<Editable placeholder="name" />
+					<Flex
+						p="1"
+						wrap="wrap"
+						gap={{ base: "8", md: "12", lg: "16" }}
+					>
+						<Editable
+							name="name"
+							label="Name"
+							value={fields.name}
+							onChange={handleChange}
+						/>
+						<Editable
+							name="email"
+							label="Email"
+							value={fields.email}
+							onChange={handleChange}
+						/>
+						<Editable
+							name="password"
+							label="Password"
+							value={fields.password}
+							onChange={handleChange}
+						/>
+						<EditableSelect
+							name="gender"
+							label="Gender"
+							value={fields.gender}
+							onChange={handleChange}
+							options={[
+								{ value: "male", text: "Male" },
+								{ value: "female", text: "Female" },
+								{ value: "none-binary", text: "None binary" },
+							]}
+						/>
+						<Editable
+							name="date_of_birth"
+							label="Date of birth"
+							type="date"
+							value={fields.date_of_birth}
+							onChange={handleChange}
+						/>
+						;
 					</Flex>
 				)}
 			</Box>
