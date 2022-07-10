c3ada9114bff170e977ac64780b2ca1528d1fd25
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-07-10 19:11:31 +0500

    request upgrade

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,13 +1,18 @@
 import storage from "./storage";
 
 const request = async (url, options) => {
-	return await fetch(`${import.meta.env.VITE_APP_API_URL}${url}`, {
+	const req = await fetch(`${import.meta.env.VITE_APP_API_URL}${url}`, {
 		headers: {
 			"Content-Type": "application/json",
 			Authorization: `Bearer ${storage.getItem("accessToken")}`,
 		},
 		...options,
 	});
+
+	if (req.status !== 200 && req.status !== 201 && req.status !== 302) {
+		throw new Error("Request failed", { cause: req.status });
+	}
+	return req.json();
 };
 
 export default request;
