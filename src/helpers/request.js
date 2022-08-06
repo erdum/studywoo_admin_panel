21582aeccdb69adab9847c90f002e607b5360e51
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-08-07 02:39:00 +0500

    handling network errors with global toast

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -1,21 +1,28 @@
 import storage from "./storage";
 
-const request = async (url, options) => {
-	const req = await fetch(`${import.meta.env.VITE_APP_API_URL}${url}`, {
-		headers: {
-			Authorization: `Bearer ${storage.getItem("accessToken")}`,
-		},
-		...options,
-	});
-
-	if (req.status !== 200) {
-		throw new Error("Request failed", { cause: req.status });
-	}
+const request = async (url, showError, options) => {
 	try {
+		const req = await fetch(`${import.meta.env.VITE_APP_API_URL}${url}`, {
+			headers: {
+				Authorization: `Bearer ${storage.getItem("accessToken")}`,
+			},
+			...options,
+		});
+
+		if (req.status !== 200 && req.status !== 201) {
+			throw new Error("Request failed", { cause: req.status });
+		}
+
 		const data = await req.json();
 		return data ?? null;
-	} catch {
-		return null;
+	} catch (error) {
+
+		// detecting client side network error
+		if (error.cause === undefined) {
+			showError("Network error! check your Internet connection");
+		} else {
+			showError("Request failed from the server !");
+		}
 	}
 };
 
