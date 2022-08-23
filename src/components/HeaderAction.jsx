788fb4c5b1d5ec6d04df68e5d0c59578858ba820
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-08-24 00:39:17 +0500

    avatar coming from global state

--- src/components/HeaderAction.jsx
+++ src/components/HeaderAction.jsx
@@ -4,25 +4,22 @@ import useStateContext from "../contexts/StateContextProvider";
 
 const HeaderAction = ({ onClick }) => {
 	const {
-		userData: { avatar, name },
+		userData: { name },
+		userAvatar,
 	} = useStateContext();
 
 	return (
 		<Tooltip hasArrow label="profile settings">
 			<div
 				onClick={onClick}
-				className={
-					"flex items-center gap-2 text-gray-500 font-semibold cursor-pointer rounded-md z-10 px-4 py-1 hover:bg-gray-100"
-				}
+				className="flex items-center gap-2 text-gray-500 font-semibold cursor-pointer rounded-md z-10 px-4 py-1 hover:bg-gray-100"
 			>
 				<img
-					src={
-						`${import.meta.env.VITE_APP_IMG_URL}${avatar}.webp` ??
-						"./avatar_placeholder.jpg"
-					}
+					src={userAvatar ?? "./avatar_placeholder.jpg"}
 					className={
 						"hidden w-10 aspect-square rounded-full object-cover lg:block"
 					}
+					alt="avatar"
 				/>
 				<p className={"hidden lg:block"}>{name ?? "Not logged in"}</p>
 			</div>
