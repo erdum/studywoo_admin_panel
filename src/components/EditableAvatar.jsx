fc6f43d3630dc0111831a02875ee3e7b815600ac
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-08-24 01:09:06 +0500

    derived state removed

--- src/components/EditableAvatar.jsx
+++ src/components/EditableAvatar.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect } from "react";
+import { useRef } from "react";
 
 import {
 	FormControl,
@@ -14,25 +14,13 @@ import { FaPen } from "react-icons/fa";
 
 const EditableAvatar = ({ label, name, onChange, src }) => {
 	const avatarRef = useRef(null);
-	const [data, setData] = useState(null);
-
-	useEffect(() => {
-		if (src && typeof src === "object") {
-			setData(URL.createObjectURL(src));
-			return () => URL.revokeObjectURL(data);
-		}
-
-	}, [src]);
 
 	const handleFileSelect = () => {
 		avatarRef.current.click();
 	};
 
 	return (
-		<FormControl
-			w={{ base: "100%", md: "45%", lg: "64" }}
-			pb={{ base: "12" }}
-		>
+		<FormControl w={{ base: "100%", md: "45%", lg: "64" }} pb={{ base: "12" }}>
 			<FormLabel
 				fontSize={{ base: "sm", md: "md" }}
 				color="gray.500"
@@ -44,7 +32,7 @@ const EditableAvatar = ({ label, name, onChange, src }) => {
 			<InputGroup cursor="pointer">
 				<InputLeftElement>
 					<Avatar
-						src={data ?? `${import.meta.env.VITE_APP_IMG_URL}${src}.webp`}
+						src={src}
 						size={{ base: "md", lg: "lg" }}
 						ml={{ base: "2", lg: "6" }}
 						mt={{ base: "4", lg: "8" }}
@@ -59,11 +47,7 @@ const EditableAvatar = ({ label, name, onChange, src }) => {
 					onChange={(event) => onChange(event.target.files)}
 				/>
 				<InputRightElement onClick={handleFileSelect}>
-					<Icon
-						color="gray.500"
-						as={FaPen}
-						mt={{ base: "4", lg: "8" }}
-					/>
+					<Icon color="gray.500" as={FaPen} mt={{ base: "4", lg: "8" }} />
 				</InputRightElement>
 			</InputGroup>
 		</FormControl>
