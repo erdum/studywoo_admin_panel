1930951d3ec92a97ffa45caadc7516b7e08495dc
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-09-20 22:14:08 +0500

    Solved Login page route added

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,15 @@
 import AccountSettings from "../pages/AccountSettings";
 import Home from "../pages/Home";
+import Login from "../components/Login";
 
 const routes = [
 	{
 		path: "/",
 		element: <Home />,
+	},
+	{
+		path: "/login",
+		element: <Login />
 	},
 	{
 		path: "/profile-settings",
