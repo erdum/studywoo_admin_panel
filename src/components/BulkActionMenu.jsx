be45e1904d4503cf46a651b0ae2bc8054c65db50
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-10-18 21:59:22 +0500

    Fix menu overlaying issue on mobile devices

--- src/components/BulkActionMenu.jsx
+++ src/components/BulkActionMenu.jsx
@@ -5,42 +5,55 @@ import {
 	MenuList,
 	MenuItem,
 	MenuDivider,
+	Box,
 } from "@chakra-ui/react";
-import { ChevronDownIcon, DeleteIcon, EditIcon, DownloadIcon } from "@chakra-ui/icons";
+import {
+	ChevronDownIcon,
+	DeleteIcon,
+	EditIcon,
+	DownloadIcon,
+} from "@chakra-ui/icons";
 
 const BulkActionMenu = () => {
 	return (
-		<Menu>
-			<MenuButton
-				size={{ base: "sm", md: "md" }}
-				color="white"
-				bg="custom.primary"
-				boxShadow="base"
-				borderWidth="2px"
-				borderColor="custom.primary"
-				ml={{ lg: "auto" }}
-				_hover={{
-					boxShadow: "none",
-					backgroundColor: "white",
-					color: "custom.primary",
-				}}
-				_active={{
-					bg: "white",
-					color: "custom.primary",
-					outline: "none"
-				}}
-				as={Button}
-				rightIcon={<ChevronDownIcon />}
-			>
-				Bulk Actions
-			</MenuButton>
-			<MenuList>
-				<MenuItem icon={<EditIcon w="4" h="4" color="gray.500" />}>Edit</MenuItem>
-				<MenuItem icon={<DeleteIcon w="4" h="4" color="gray.500" />}>Delete</MenuItem>
-				<MenuDivider />
-				<MenuItem icon={<DownloadIcon w="4" h="4" color="gray.500" />}>Export</MenuItem>
-			</MenuList>
-		</Menu>
+		<Box zIndex="10" ml={{ lg: "auto" }}>
+			<Menu>
+				<MenuButton
+					size={{ base: "sm", md: "md" }}
+					color="white"
+					bg="custom.primary"
+					boxShadow="base"
+					borderWidth="2px"
+					borderColor="custom.primary"
+					_hover={{
+						boxShadow: "none",
+						backgroundColor: "white",
+						color: "custom.primary",
+					}}
+					_active={{
+						bg: "white",
+						color: "custom.primary",
+						outline: "none",
+					}}
+					as={Button}
+					rightIcon={<ChevronDownIcon />}
+				>
+					Bulk Actions
+				</MenuButton>
+				<MenuList>
+					<MenuItem icon={<EditIcon w="4" h="4" color="gray.500" />}>
+						Edit
+					</MenuItem>
+					<MenuItem icon={<DeleteIcon w="4" h="4" color="gray.500" />}>
+						Delete
+					</MenuItem>
+					<MenuDivider />
+					<MenuItem icon={<DownloadIcon w="4" h="4" color="gray.500" />}>
+						Export
+					</MenuItem>
+				</MenuList>
+			</Menu>
+		</Box>
 	);
 };
 
