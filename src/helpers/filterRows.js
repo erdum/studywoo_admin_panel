a21bdfd90ebc0e1c0c6ee58616f386474cd4c652
Erdum <erdumadnan@gmail.com>
2022-10-20 10:12:35 +0500

    Update searing case insensitive

--- src/helpers/filterRows.js
+++ src/helpers/filterRows.js
@@ -2,7 +2,7 @@ const filterRows = (rows, searchValue) => {
 	return rows?.filter((row) => {
 		const values = Object.values(row);
 		return values.find((value) => {
-			const word = String(value);
+			const word = String(value).toLowerCase();
 			return word.includes(searchValue);
 		});
 	}) ?? [];
