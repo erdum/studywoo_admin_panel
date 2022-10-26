b9bd46c76ff8ec15fbe7001597e5307766858d5e
Syed M Erdum Adnan <erdumadnan@gmail.com>
2022-10-26 21:29:22 +0500

    Update search modal component established

--- src/components/app_shell/SearchModal.jsx
+++ src/components/app_shell/SearchModal.jsx
@@ -1,40 +1,57 @@
-import { Modal, ModalOverlay, ModalContent, ModalBody } from "@chakra-ui/react";
-import { InputGroup, InputLeftElement, Input, Icon } from "@chakra-ui/react";
+import {
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalBody,
+	Input,
+	Icon,
+	Box,
+} from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
 
 const SearchBar = ({ onChange }) => {
 	return (
-		<InputGroup>
-			<InputLeftElement
-				h="100%"
-				pointerEvents="none"
-				children={<Icon color="gray.500" fontSize="2xl" as={FaSearch} />}
+		<Box
+			display="flex"
+			alignItems="center"
+			justifyContent="space-between"
+			px={{ base: "4", md: "8" }}
+		>
+			<Icon
+				color="gray.500"
+				fontSize={{ base: "xl", md: "2xl" }}
+				mr="2"
+				as={FaSearch}
 			/>
 			<Input
 				focusBorderColor="custom.primary"
 				type="search"
-				placeholder="search"
-				h="16"
+				placeholder="Search"
+				h={{ base: "12", md: "16" }}
+				fontSize={{ base: "md", md: "2xl" }}
+				fontWeight={{ md: "semibold" }}
+				border="none"
 				_focusVisible={{
-					outline: "none"
+					outline: "none",
 				}}
+				onChange={({ target: { value } }) => onChange(value)}
 			/>
-		</InputGroup>
+		</Box>
 	);
 };
 
 const SearchModal = ({ isOpen, onClose }) => {
-  return (
-    <Modal size="xl" isOpen={isOpen} onClose={onClose}>
+	return (
+		<Modal size={{ base: "xs", md: "xl" }} isOpen={isOpen} onClose={onClose}>
 			<ModalOverlay>
 				<ModalBody>
 					<ModalContent display="flex" alignItems="stretch">
-						<SearchBar />
+						<SearchBar onChange={(value) => console.log(value)} />
 					</ModalContent>
 				</ModalBody>
 			</ModalOverlay>
 		</Modal>
-  );
+	);
 };
 
 export default SearchModal;
