8d86d77979704d64ceccbc408a0aaad9cd1f298d
Erdum <erdumadnan@gmail.com>
2022-11-07 09:44:32 +0500

    Update menu items added

--- src/Menu-Items.js
+++ src/Menu-Items.js
@@ -1,10 +1,43 @@
-import { FaUserCog, FaRegEnvelope } from "react-icons/fa";
+import {
+	FaUserCog,
+	FaRegEnvelope,
+	FaUserAlt,
+	FaUserEdit,
+	FaCommentAlt,
+	FaQuestionCircle,
+	FaClipboardList,
+} from "react-icons/fa";
 
 const MenuLinks = [
 	{
 		path: "/",
 		label: "Applications",
 		icon: FaRegEnvelope,
+	},
+	{
+		path: "/users",
+		label: "Users",
+		icon: FaUserAlt,
+	},
+	{
+		path: "/roles",
+		label: "Roles",
+		icon: FaUserEdit,
+	},
+	{
+		path: "/comments",
+		label: "Comments",
+		icon: FaCommentAlt,
+	},
+	{
+		path: "/faqs",
+		label: "Faqs",
+		icon: FaQuestionCircle,
+	},
+	{
+		path: "/ratings",
+		label: "Ratings",
+		icon: FaClipboardList,
 	},
 	{
 		path: "/profile-settings",
