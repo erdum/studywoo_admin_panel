d4f2c4a50561696e6d1f6ff2da814934ef701e8a
Erdum <erdumadnan@gmail.com>
2022-11-25 16:59:30 +0500

    Update data management lifted

--- src/pages/AccountSettings.jsx
+++ src/pages/AccountSettings.jsx
@@ -1,60 +1,58 @@
-import { useEffect, useState } from "react";
+import FieldsPage from "../components/page_comps/FieldsPage";
 
-const fieldsStructure = {
-    name: "",
-    email: "",
-    password: "",
-    avatar: "",
-    gender: "",
-    date_of_birth: "",
-    about: "",
-    facebook: "",
-    instagram: "",
-    twitter: "",
-    linkedin: "",
-};
+const fieldsStructure = [
+    {
+        name: "name",
+        label: "Name",
+    },
+    {
+        name: "email",
+        label: "Email",
+    },
+    {
+        name: "password",
+        label: "Password",
+    },
+    {
+        name: "avatar",
+        label: "Profile picture",
+        type: "image",
+    },
+    {
+        name: "gender",
+        label: "Gender",
+        type: "single_select",
+    },
+    {
+        name: "date_of_birth",
+        label: "Date of birth",
+        type: "date",
+    },
+    {
+        name: "about",
+        label: "About",
+        type: "rich_text",
+    },
+    {
+        name: "facebook",
+        label: "Facebook",
+    },
+    {
+        name: "instagram",
+        label: "Instagram",
+    },
+    {
+        name: "twitter",
+        label: "Twitter",
+    },
+    {
+        name: "linkedin",
+        label: "Linkedin",
+    },
+];
 
 const AccountSettings = () => {
-    const { isFetching, data, updateFields, isFieldsUploading } =
-        syncFieldsWithServer("managment/user-profile", fieldsStructure);
-
-    const {
-        userData: { avatar },
-    } = useStateContext();
-
-    const [fields, setFields] = useState({ ...data, changed: false });
-
-    useEffect(() => {
-        data ? setFields({ ...data, changed: false }) : null;
-    }, [data]);
-
-    const handleSave = async () => {
-        if (fields.avatar && typeof fields.avatar === "object") {
-            const avatar = new FormData();
-            const extension = fields.avatar.name.split(".").at(-1);
-            avatar.append(
-                "images",
-                fields.avatar,
-                `${fields.email}.${extension}`
-            );
-            updateImage({ url: "pilot_upload", image: avatar });
-        }
-        // updateProfile({ ...fields, avatar: fields.email });
-    };
-
-    const handleRichText = (text) => {
-        if (text !== fields.about) {
-            setFields((prevState) => ({
-                ...prevState,
-                changed: true,
-                about: text,
-            }));
-        }
-    };
-
-    return (
-        
-    );
+    return <FieldsPage structure={fieldsStructure} />;
 };
 
 export default AccountSettings;
