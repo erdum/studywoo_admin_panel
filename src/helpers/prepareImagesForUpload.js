d18b0a959348eae892319650cce0ba382dbf8eed
Erdum <erdumadnan@gmail.com>
2022-11-26 09:34:34 +0500

    Update return type modified to single object

--- src/helpers/prepareImagesForUpload.js
+++ src/helpers/prepareImagesForUpload.js
@@ -1,6 +1,6 @@
 const prepareImagesForUpload = (fields) => {
     const payloadArray = [];
-    const names = [];
+    const names = {};
     Object.entries(fields).filter(([fieldName, file]) => {
         if (file?.constructor != File) return false;
 
@@ -9,7 +9,7 @@ const prepareImagesForUpload = (fields) => {
         const extension = file.name.split(".").at(-1);
 
         payload.append("images", file, `${fileName}.${extension}`);
-        names.push({ [fieldName]: fileName });
+        names[fieldName] = fileName;
         payloadArray.push(payload);
     });
 
