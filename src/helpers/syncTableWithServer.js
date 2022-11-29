f6394e16140ee9bd699ed738971ed6f0df2fe191
Erdum <erdumadnan@gmail.com>
2022-11-29 23:32:43 +0500

    Update mechanism added for adding row into table

--- src/helpers/syncTableWithServer.js
+++ src/helpers/syncTableWithServer.js
@@ -60,6 +60,7 @@ const syncTableWithServer = (resourcePath) => {
                 });
 
                 const prevPayload = queryClient.getQueryData(resourcePath);
+
                 queryClient.setQueryData(resourcePath, (prevData) =>
                     prevData.map((row) => {
                         const rowNeedsToUpdate = payload.rows[0];
@@ -86,11 +87,50 @@ const syncTableWithServer = (resourcePath) => {
         }
     );
 
+    const { mutate: addRow } = useMutation(
+        async (payload) =>
+            request(resourcePath, showAppToast, {
+                body: payload,
+                method: "POST",
+            }),
+        {
+            onMutate: async (payload) => {
+                await queryClient.cancelQueries({
+                    queryKey: resourcePath,
+                });
+
+                const prevPayload = queryClient
+                    .getQueryData(resourcePath)
+                    .filter(({ id }) => id != payload?.id);
+
+                return { prevPayload };
+            },
+            onSuccess: () => queryClient.invalidateQueries(resourcePath),
+            onError: (err, newPayload, { prevPayload }) => {
+                queryClient.setQueryData(resourcePath, prevPayload);
+            },
+        }
+    );
+
+    const addRowInCache = (row) =>
+        queryClient.setQueryData(resourcePath, (prevData) => [
+            ...prevData,
+            row,
+        ]);
+
+    const removeRowFromCache = (id) =>
+        queryClient.setQueryData(resourcePath, (prevData) =>
+            prevData.filter(({ id: rowId }) => rowId != id)
+        );
+
     return {
         isFetching,
         data,
         deleteRows,
         updateRow,
+        addRow,
+        addRowInCache,
+        removeRowFromCache,
     };
 };
 
