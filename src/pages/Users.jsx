90bdfc1cf3096553c01ae9566c5cd63a8775f009
Erdum <erdumadnan@gmail.com>
2022-12-04 23:12:25 +0500

    Update dynamic options implemented

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,55 +1,66 @@
+import getOptions from "../helpers/getOptions";
+import { singleSelect } from "../helpers/customColumns";
+
 // Custom Components
 import TablePage from "../components/page_comps/TablePage";
 
-// Table Columns Definition
-const columns = [
-    {
-        field: "id",
-        headerName: "ID",
-        width: 140,
-        editable: true,
-    },
-    {
-        field: "name",
-        headerName: "Name",
-        width: 400,
-        editable: true,
-    },
-    {
-        field: "email",
-        headerName: "Email",
-        width: 400,
-        editable: true,
-    },
-    {
-        field: "password",
-        headerName: "Password",
-        width: 400,
-        editable: true,
-    },
-    {
-        field: "roles",
-        headerName: "User Roles",
-        width: 200,
-        editable: true,
-    },
-    {
-        field: "status",
-        headerName: "User Status",
-        width: 110,
-        editable: true,
-        type: "boolean",
-    },
-];
+const Users = () => {
+    const { options: rolesOptions } = getOptions("managment/roles", "roles");
 
-const Users = () => (
-    <TablePage
-        columns={columns}
-        resourcePath="managment/users"
-        title="Users"
-        description="Manage system admins and users"
-        btnText="Save"
-    />
-);
+    // Table Columns Definition
+    const columns = [
+        {
+            field: "id",
+            headerName: "ID",
+            width: 140,
+            editable: true,
+        },
+        {
+            field: "name",
+            headerName: "Name",
+            width: 400,
+            editable: true,
+        },
+        {
+            field: "email",
+            headerName: "Email",
+            width: 400,
+            editable: true,
+        },
+        {
+            field: "password",
+            headerName: "Password",
+            width: 400,
+            editable: true,
+        },
+        {
+            field: "roles",
+            headerName: "User Roles",
+            width: 200,
+            editable: true,
+            ...singleSelect,
+            valueOptions: rolesOptions,
+        },
+        {
+            field: "status",
+            headerName: "User Status",
+            width: 110,
+            editable: true,
+            type: "boolean",
+        },
+    ];
+
+    console.log(rolesOptions);
+
+    return (
+        <TablePage
+            columns={columns}
+            resourcePath="managment/users"
+            title="Users"
+            description="Manage system admins and users"
+            btnText="Save"
+        />
+    );
+};
 
 export default Users;
