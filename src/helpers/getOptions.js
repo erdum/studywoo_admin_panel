eeb43ede89938fbfb4ce378d18e15e29aff6e137
Erdum <erdumadnan@gmail.com>
2022-12-04 23:11:23 +0500

    Update select field dynamic options mechanism

--- src/helpers/getOptions.js
+++ src/helpers/getOptions.js
@@ -1,3 +1,28 @@
-const getOptions = () => {};
+import { useQuery } from "react-query";
+
+// Helper functions
+import request from "./request";
+
+// App State Context
+import useStateContext from "../contexts/StateContextProvider";
+
+const getOptions = (resourcePath, columnName) => {
+    const { showAppToast } = useStateContext();
+
+    const { data, isFetching } = useQuery(
+        resourcePath,
+        async ({ queryKey }) => request(queryKey, showAppToast),
+        {
+            retry: 0,
+            refetchOnWindowFocus: false,
+            initialData: [],
+        }
+    );
+
+    return {
+        options: data.map(row => row[columnName]),
+        isOptionsLoading: isFetching,
+    };
+};
 
 export default getOptions;
