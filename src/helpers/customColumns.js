9426335b57d9ec2604b88d611bbd5fca71b529b5
Erdum <erdumadnan@gmail.com>
2022-12-05 20:45:26 +0500

    Update custom date column added

--- src/helpers/customColumns.js
+++ src/helpers/customColumns.js
@@ -9,9 +9,20 @@ const richText = {};
 
 const file = {};
 
+const date = {
+    type: "date",
+    valueFormatter: ({ value }) => {
+        if (!value) return '';
+
+        const date = new Date(value);
+        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    },
+};
+
 export {
     singleSelect,
     multiSelect,
     richText,
     file,
+    date,
 };
